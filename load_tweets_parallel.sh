d541806410755f0c7d2412dafdbd72c7900ac04d
lucasLang0 <lucas.lang@gmail.com>
2025-05-04 22:19:21 -0700

    first

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -16,7 +16,7 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: copy your solution to the previous problem here
-
+echo "$files" | time parallel sh load_denormalized.sh
 # NOTE:
 # I have removed the pg_normalized code from this repo.
 # The only difference between pg_normalized and pg_normalized_batch is how the data is loaded.
@@ -27,3 +27,4 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: copy your solution to the previous problem here
+# #echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1052/ --inputs
