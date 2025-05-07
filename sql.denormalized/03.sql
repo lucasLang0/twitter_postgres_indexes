27f1003f64cdf598ded37bf0c91ea5948e9173f8
lucasLang0 <lucas.lang@gmail.com>
2025-05-06 22:40:44 -0700

    large sad

--- sql.denormalized/03.sql
+++ sql.denormalized/03.sql
@@ -1,8 +1,10 @@
-SELECT
-    lang,
-    count(DISTINCT id_tweets) as count
-FROM tweet_tags
-JOIN tweets USING (id_tweets)
-WHERE tag='#coronavirus'
+SELECT lang, count(*) AS count
+FROM (
+    SELECT distinct data->>'id' AS id_tweets, data->>'lang' AS lang
+    FROM tweets_jsonb
+    WHERE data ->'entities'->'hashtags'@@'$[*].text == "coronavirus"' 
+	OR data->'extended_tweet'->'entities'->'hashtags'@@'$[*].text == "coronavirus"'
+) t1
 GROUP BY lang
-ORDER BY count DESC,lang;
+ORDER BY count DESC, lang
+LIMIT 1000;
